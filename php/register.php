318bf992765dd50d51e9c79e866e3bd8f455be4c
Nicolas5150 <Nicolas@Nicolas-MacBook-Pro.local>
2016-11-23 17:28:55 -0500

    Survey Validation - Cont.
    Survey now finds highest count and appends the image correlating to it.
    It also shows the user his or her profile info again and a link to
    download the image via a relative link.
    Next is to show the user all the answers that were entered in from the
    survey.

--- php/register.php
+++ php/register.php
@@ -10,12 +10,12 @@
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="stylesheet.css" media="screen"/>
 		<title>Project 4</title>
-		
-	</head>	
-	
+
+	</head>
+
 	<body>
 <?php include("db_group4.php"); ?>
-	
+
 <center>
 		<div class="forms">
 			<h1>Create your account here!</h1>
@@ -24,28 +24,28 @@
 					<p><span class="error">* required field</span></p><br><br>
 							<input name="username" type="text" placeholder="Username" class="box" value="">
 								<span class="error">* </span><br>
-						
+
 							<input name="password" type="password" placeholder="Password" class="box" value="">
 								<span class="error">* </span><br>
-							
+
 							<input name="firstname" type="text" placeholder="First Name" class="box" value="">
 								<span class="error">* </span><br>
-							
+
 							<input name="lastname" type="text" placeholder="Last Name" class="box" value="">
 								<span class="error">* </span><br>
-							
+
 							<input name="phone" type="text" placeholder="Phone Number" size="10" maxlength="10" class="box" value="">
 								<span class="error">* </span><br>
-							
+
 							<input name="email" type="text" placeholder="Email" class="box" value="">
 								<span class="error">* </span><br>
-							
+
 							<input type="submit" name="register" class="submit" value="Register"><br>
-				
-			
-			
+
+
+
 					</form>
-	
+
 <?php
 	if(isset($_POST['register'])){		//if create account submit button is pressed.
 		$username = $_POST['username'];
@@ -54,7 +54,7 @@
 		$lastname = $_POST['lastname'];
 		$phone = $_POST['phone'];
 		$email = $_POST['email'];
-		
+
 	    if (empty($username)) {
           print '<h5 class= "err">Username is required.</h5>';
         } else {
@@ -66,51 +66,68 @@
           $validpassword = "";
         }
 		if (!preg_match("/^[a-zA-Z0-9]*$/",$password)) {
-        } 
+        }
 	if (empty($firstname)) {
           print '<h5 class= "err">Enter your first name.</h5>';
         } else {
           $validfirstname = "";
-        }	
+        }
 	if (empty($lastname)) {
           print '<h5 class= "err">Enter your first name.</h5>';
         } else {
           $validlastname = "";
         }
-	
-        	
+
+
     $phone = preg_replace("/[\)\(\s-.]/", '', $phone);
 	if (!preg_match("/^[0-9]{10}/", $phone))  {
-		print '<h5 class= "err">Phone must be 10 numbers</h5>'; 
+		print '<h5 class= "err">Phone must be 10 numbers</h5>';
         }else{
           $validphone = "";
         }
 	if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-		print '<h5 class= "err">Invalid email format.</h5>'; 
+		print '<h5 class= "err">Invalid email format.</h5>';
 		}else {
 		  $validemail = "";
-		}	
-    
- if (isset($validusername) && isset($validpassword) && isset($validfirstname) && isset($validlastname) && isset($validphone) && isset($validemail)) {	   
-  
-		
+		}
 
-		$sql = "SELECT * FROM group4 WHERE username='".$username."' LIMIT 1";	//checks the server for a duplicate of the user defined username.
+ if (isset($validusername) && isset($validpassword) && isset($validfirstname) && isset($validlastname) && isset($validphone) && isset($validemail)) {
+
+		$sql = "SELECT * FROM user WHERE username='".$username."' LIMIT 1";	//checks the server for a duplicate of the user defined username.
 		$result = mysqli_query($connection, $sql);
-		
+
 		if (mysqli_num_rows($result) == 1){		//if username already exists on the server...
 			echo "<h5 class='loginerr'>Username taken. Choose another Username.</h5>";
-			
-		
+
+
 		} else {								//if the username does not exist
-			$sql = "INSERT INTO group4 (username,password,firstname,lastname,phone,email) VALUES ('$username','$password','$firstname','$lastname','$phone','$email')";			//creates a new entry in account table containing the user defined variable inputs...
+			$sql = "INSERT INTO user (username,password,firstname,lastname,phone,email) VALUES ('$username','$password','$firstname','$lastname','$phone','$email')";			//creates a new entry in account table containing the user defined variable inputs...
 			mysqli_query($connection, $sql);
 			//$_SESSION['createdacccount'] = 1;		//sets acount created session variable equal to 1.
 			echo "<h5 class='loginerr'>Your account is created! Directing to the Login form.</h5>";
-			header("Refresh: 3; url=login.php");		//redirects to the login page.
+
+      // Create a new table in the database corresponding to the new user created.
+      $newTable = " CREATE TABLE $username (
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        firstname VARCHAR(30) NOT NULL,
+        lastname VARCHAR(30) NOT NULL,
+        email VARCHAR(50),
+        phone VARCHAR(50),
+        carrier VARCHAR(50)
+        )";
+
+      $tableResult = mysqli_query($connection, $newTable);
+      if ($tableResult === TRUE) {
+        echo"it worked!";
+      }
+      else{
+        echo"nope";
+      }
+
+      //header("Refresh: 3; url=login.php");		//redirects to the login page.
 		}
-		
-	} 
+
+	}
 }
 ?>
 </center>
